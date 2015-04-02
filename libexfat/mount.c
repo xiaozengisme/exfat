3a2ff5df8c9ed9d1e50cf33a37055b5a830f1fbc
relan <relan@users.noreply.github.com>
2015-04-02 11:45:49 +0300

    Remove dmask and fmask options.
    They are not supported by FUSE and thus require special handling (cannot
    be just left in the mount options string). Besides, there are hardly any
    users of those options.

--- libexfat/mount.c
+++ libexfat/mount.c
@@ -93,15 +93,9 @@ static bool match_option(const char* options, const char* option_name)
 
 static void parse_options(struct exfat* ef, const char* options)
 {
-	int opt_umask;
-
-	opt_umask = get_int_option(options, "umask", 8, 0);
-	ef->dmask = get_int_option(options, "dmask", 8, opt_umask);
-	ef->fmask = get_int_option(options, "fmask", 8, opt_umask);
-
+	ef->dmask = ef->fmask = get_int_option(options, "umask", 8, 0);
 	ef->uid = get_int_option(options, "uid", 10, geteuid());
 	ef->gid = get_int_option(options, "gid", 10, getegid());
-
 	ef->noatime = match_option(options, "noatime");
 }
 
